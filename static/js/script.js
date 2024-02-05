9a46c0cda4f695c8ece2f11e8e7ce92805d32dea
Björn Englesson <benglesson@gmail.com>
2024-02-05 19:45:50 +0100

    hardcode default value dark

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,17 @@
 const setTheme = (theme) => {
   document.documentElement.className = theme;
   localStorage.setItem('theme', theme);
+  console.log('theme set to', theme);
 }
 
 const hasCodeRun = localStorage.getItem('hasCodeRun');
 
 if (!hasCodeRun) {
-  const defaultTheme = "{{ config.extra.default_theme }}";
+  const defaultTheme = "dark";
   setTheme(defaultTheme);
   localStorage.setItem('hasCodeRun', 'true');
 }
+
 
 const getTheme = () => {
   const theme = localStorage.getItem('theme');
